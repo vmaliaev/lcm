bd7da2d937909e31915929543540f598e8bd568d
root <root@fuel.domain.tld>
2016-03-03 20:40:09 +0000

    Setup Foreman

---
+++ site1.pp
@@ -0,0 +1,63 @@
+class { '::foreman':
+  db_type => mysql,
+  db_host => localhost,
+  db_port => 3306,
+  db_manage => true,
+  db_adapter => mysql2,
+  db_database => foreman,
+  db_username => foreman,
+  db_password => 'changeme',
+  admin_username => admin,
+  admin_password => 'changemea',
+  authentication => true,
+#apipie_task => 'apipie:cache',
+  oauth_active => true,
+  oauth_map_users => true,
+  oauth_consumer_key => WvZpMupBEGZPj6RXXZBprvUKg6kSNM5e,
+  oauth_consumer_secret => "EiwMYSN78wKxQETrqB7rhCWBn9HcXV3x",
+  passenger	=> true,
+    apipie_task => "apipie:cache:index",
+    app_root	=> "/usr/share/foreman",
+    passenger_prestart	=> true,
+    passenger_min_instances	=> "1",
+    passenger_start_timeout	=> "600",
+  environment=> production,
+  user => foreman,
+  group => foreman,
+  #passenger => true,
+  puppetrun => true,
+}
+
+class { '::foreman::puppetmaster':
+foreman_user => 'admin',
+foreman_password => 'changemea',
+#passenger => true,
+
+}
+
+class { '::foreman_proxy':
+  tftp => false,
+  plugin_version => absent,
+  ssl => true,
+  http_port => 8000,
+  ssl_port => 8443,
+  puppetrun => true,
+  dhcp => false,
+  dns => false,
+  bmc => false,
+  realm => false,
+# puppet_use_environment_api => true,
+# registered_proxy_url => 'https://node-17.domain.tld:8443',
+# registered_name => 'localhost',
+  register_in_foreman => true,
+  foreman_base_url => "https://node-15.domain.tld",
+  oauth_consumer_key => WvZpMupBEGZPj6RXXZBprvUKg6kSNM5e,
+  oauth_consumer_secret => "EiwMYSN78wKxQETrqB7rhCWBn9HcXV3x",
+  registered_name => 'node-15.domain.tld',
+  registered_proxy_url => "https://node-15.domain.tld:8443",
+  oauth_effective_user => admin,
+  dir => "/usr/share/foreman-proxy",
+  user => foreman-proxy,
+ 
+}
+
