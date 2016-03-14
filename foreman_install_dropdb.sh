ef643dfd5f7c65840843ab403348128ec2ce25b2
root <root@fuel.domain.tld>
2016-03-14 18:37:21 +0000

    Delete notify bad string

--- foreman_install_dropdb.sh
+++ foreman_install_dropdb.sh
@@ -6,6 +6,4 @@ git clone https://github.com/vmaliaev/lcm
 
 cp -r ./lcm/foreman_ext /etc/fuel/plugins/fuel-plugin-lcm-2.0/puppet/modules/ ; cp ./lcm/foreman_main.pp /etc/fuel/plugins/fuel-plugin-lcm-2.0/puppet/manifests/
 
-notify {'run apipie': notify => Foreman::Rake["$::foreman::apipie_task"], }
-
 puppet apply --modulepath=/etc/fuel/plugins/fuel-plugin-lcm-2.0/puppet/modules:/etc/puppet/modules /etc/fuel/plugins/fuel-plugin-lcm-2.0/puppet/manifests/foreman_main.pp
