e8e0f3e1c7d7c3baac00f9d5520a4e035766f699
root <root@fuel.domain.tld>
2016-03-20 18:21:46 +0000

    Fix script

--- foreman_install_dropdb.sh
+++ foreman_install_dropdb.sh
@@ -4,7 +4,9 @@ apt-get install git -y
 
 git clone https://github.com/vmaliaev/lcm
 
+a=`ls /etc/fuel/plugins/`
 #cp -r ./lcm/foreman_ext /etc/fuel/plugins/fuel-plugin-lcm-2.0/puppet/modules/ ; cp ./lcm/foreman_main.pp /etc/fuel/plugins/fuel-plugin-lcm-2.0/puppet/manifests/
-cp -r ./lcm/deployment_scripts/puppet/modules/plugin_lcm/manifests/foreman_ext.pp /etc/fuel/plugins/fuel-plugin-lcm-2.0/puppet/modules/plugin_lcm/manifests/ ; cp ./lcm/foreman_main.pp /etc/fuel/plugins/fuel-plugin-lcm-2.0/puppet/manifests/
+#cp -r ./lcm/deployment_scripts/puppet/modules/plugin_lcm/manifests/foreman_ext.pp /etc/fuel/plugins/${a}/puppet/modules/plugin_lcm/manifests/ 
+cp ./lcm/foreman_main.pp /etc/fuel/plugins/${a}/puppet/manifests/
 
 puppet apply --modulepath=/etc/fuel/plugins/fuel-plugin-lcm-2.0/puppet/modules:/etc/puppet/modules /etc/fuel/plugins/fuel-plugin-lcm-2.0/puppet/manifests/foreman_main.pp
