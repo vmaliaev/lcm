530a28fbe3b92ac63da4721c0cf98553ea490ecf
Vladimir Maliaev <vmaliaev@mirantis.com>
2016-06-12 07:58:05 +0000

    Add functionality

--- auto_prep_lcm.sh
+++ auto_prep_lcm.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 
+# Functions
+function install_fpb() {
+#  apt-get -y git easy_pip ....
+#  easy_pip install
+}
+
+
+# BODY
 # Preparing new environment for att, upload repos, reinstall plugin, recreate env
 
-if [ `ls * | grep -c fuel-plugin-lcm-1.0-1.0.0-1.noarch.rpm` -eq 0 ] ; then echo "ERROR: Change directory on containted fuel-plugin-lcm-1.0-1.0.0-1.noarch.rpm"; echo "Exiting..." ; exit 1 ; fi
+#install_fpb #Uncomment it on new environment
+
+if [ `ls * | grep -c fuel-plugin-lcm-1.0-1.0.0-1.noarch.rpm` -eq 0 ] ; then echo "ERROR: Change directory to containted fuel-plugin-lcm-1.0-1.0.0-1.noarch.rpm"; echo "Exiting..." ; exit 1 ; fi
 
 
 
 echo -e "\nPreparing new ENV"
 echo "-----------------"
+
+echo -e "\nArchiving targetimages:"
+mkdir -p /var/www/nailgun/targetimages/copy
+rm -f /var/www/nailgun/targetimages/copy/*
+cd /var/www/nailgun/targetimages
+env_no=`ls | grep yaml | head -1 | cut -d"_" -f2`
+mv /var/www/nailgun/targetimages/env_${env_no}_* /var/www/nailgun/targetimages/copy/
+cd -
 
 echo -e "\nDeleting all environments:"
 fuel nodes list
@@ -34,33 +52,38 @@ cd -
 echo -e "\nUpload ATT repos:"
 fuel settings --env-id $e download --dir ~/
 sed "/service_user/i \\
-      - name: mos-aic\\
+      - name: extra-0\\
+        priority: 1150\\
+        section: main\\
+        suite: trusty\\
+        type: deb\\
+        uri: http://172.18.82.139/percona/\\
+      - name: extra-1\\
         priority: 1160\\
         section: main\\
         suite: mos7.0\\
         type: deb\\
-        uri: http://10.20.0.2:8080/aic-3.0.1/mos-7.0/\\
-      - name: system-9-0\\
+        uri: http://172.18.82.139/mos-7.0/\\
+      - name: extra-2\\
         priority: 1170\\
         section: main\\
         suite: trusty\\
         type: deb\\
-        uri: http://10.20.0.2:8080/aic-3.0.1/infra-mirror-9.0-master/\\
-      - name: percona\\
-        priority: 2150\\
-        section: main\\
-        suite: trusty\\
-        type: deb\\
-        uri: http://repo.percona.com/apt" -i ~/settings_${e}.yaml
+        uri: http://172.18.82.139/infra-mirror-9.0-master/\\
+" -i ~/settings_${e}.yaml
 
 fuel settings --env-id $e upload --dir ~/
 
 echo -e "\nfuel node --env-id=${e} --node-id=2,2,2 --provision"
 
-echo -e "\nFINISH, I will be waiting 7 minutes for discovered nodes and will echo a message to provision."
+echo -e "\nFINISH, I will be waiting about 7 minutes for discovered nodes and will echo a message to provision."
+#sleep 420
+# Waiting nodes to back online:
+while [ `fuel node | grep discover | awk '{print $1}' | wc -l` -lt 3 ] ; do
+  sleep 5
+done
 
-sleep 420
-
+# Creating launch line:
 ips=""
 if [ `fuel node | grep discover | awk '{print $1}' | wc -l` -gt 2 ] ; then
   for i in `fuel node | awk /discover/'{print $1}'` ; do ips="${ips}$i,"; done ;
@@ -70,4 +93,6 @@ ips=`echo ${ips} | rev | cut -c 2- | rev`
 
 fuel node
 echo -e "\nfuel node --env-id=${e} --node-id=${ips} --provision; sleep 300; fuel node --env-id=${e} --node-id=${ips} --deploy"
+echo 'mco rpc execute_shell_command execute cmd="echo sudo -i>>/var/lib/fuel/.profile"'
+
 
